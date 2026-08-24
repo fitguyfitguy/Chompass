086c9d2f7389987a70b129187c711667a714b5b0
fitguy <fit.guy@mailfence.com>
2026-08-24 17:07:34 +0200

    style: drop unused imports (ktlint) in prompt-history + notes screens

--- android/app/src/main/java/app/chompass/ui/home/PromptHistorySheet.kt
+++ android/app/src/main/java/app/chompass/ui/home/PromptHistorySheet.kt
@@ -12,11 +12,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.History
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
--- android/app/src/main/java/app/chompass/ui/settings/NotesSettingsScreen.kt
+++ android/app/src/main/java/app/chompass/ui/settings/NotesSettingsScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import app.chompass.AppContainer
 import app.chompass.R
