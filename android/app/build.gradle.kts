fb6d820ef753d0e49ec84d45bddbcb94b0f1d0b2
fitguy <fit.guy@mailfence.com>
2026-08-27 16:18:03 +0200

    Release 4.1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -62,8 +62,8 @@ android {
         applicationId = "app.chompass"
         minSdk = 26
         targetSdk = 36
-        versionCode = 67
-        versionName = "4.0.0"
+        versionCode = 68
+        versionName = "4.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         if (releaseAbi != null) {
